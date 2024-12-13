5448d8c4c8abb5f8c545f5bc484b1d8a72a1890d
markPLP <mpclaspinas@gmail.com>
2024-12-13 16:51:39 +0800

    Create types and refactor action/createProductAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,25 @@
+'use server';
+
 import db from '@/utils/db';
 import { Product } from '@prisma/client';
 import { redirect } from 'next/navigation';
+import { auth, currentUser } from '@clerk/nextjs/server';
+import { productSchema, validateWithZodSchema } from './schema';
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : 'An error occurred',
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  return user;
+};
 
 export const fetchFeaturedProducts = async (): Promise<Product[]> => {
   const products = await db.product.findMany({
@@ -40,4 +59,27 @@ export const fetchSingleProduct = async (productId: string) => {
     redirect('/products');
   }
   return product;
+};
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(productSchema, rawData); // using schema helper function
+
+    await db.product.create({
+      data: {
+        ...validatedFields,
+        image: '/images/product-1.jpg',
+        clerkId: user.id,
+      },
+    });
+    return { message: 'product created' };
+  } catch (error) {
+    return renderError(error);
+  }
 };
