5d25c1b4cea678c246f45a0071e904744b89f1b4
markPLP <mpclaspinas@gmail.com>
2024-12-15 16:11:57 +0800

    misc fixes - update dependency version

--- middleware.ts
+++ middleware.ts
@@ -4,23 +4,17 @@ import { NextResponse } from 'next/server';
 const isPublicRoute = createRouteMatcher(['/', '/products(.*)', '/about']);
 const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 
-export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
-  const isAdminUser = userId === process.env.ADMIN_USER_ID;
+export default clerkMiddleware(async (auth, req) => {
+  // console.log(auth().userId);
 
-  // redirect to home if not admin user
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+
   if (isAdminRoute(req) && !isAdminUser) {
     return NextResponse.redirect(new URL('/', req.url));
   }
-
   if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
