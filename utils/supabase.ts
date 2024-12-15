8a79e9393786960e2b8fa020c0cf583eee95105d
markPLP <mpclaspinas@gmail.com>
2024-12-15 19:30:32 +0800

    Create deleteImage helper function

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -21,3 +21,12 @@ export const uploadImage = async (image: File) => {
   if (!data) throw new Error('Image upload failed');
   return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
+
+// split Output: ["https:", "", "example.com", "images", "photo.jpg"]
+// pop Output: "photo.jpg"
+// delete image using imagename
+export const deleteImage = (url: string) => {
+  const imageName = url.split('/').pop();
+  if (!imageName) throw new Error('Invalid URL');
+  return supabase.storage.from(bucket).remove([imageName]);
+};
