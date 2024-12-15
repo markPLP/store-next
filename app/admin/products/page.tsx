22e5126676f263ca19edd55d5f9f446b55f86567
markPLP <mpclaspinas@gmail.com>
2024-12-15 18:51:26 +0800

    Add actions UI - edit and delete

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,9 @@
 import EmptyList from '@/components/global/EmptyList';
 import { fetchAdminProducts } from '@/utils/actions';
 import Link from 'next/link';
+import FormContainer from '@/components/form/FormContainer';
+import { IconButton } from '@/components/form/Button';
+import { deleteProductAction } from '@/utils/actions';
 
 import { formatCurrency } from '@/utils/format';
 import {
@@ -46,13 +49,28 @@ async function ItemsPage() {
                 <TableCell>{company}</TableCell>
                 <TableCell>{formatCurrency(price)}</TableCell>
 
-                <TableCell className="flex items-center gap-x-2"></TableCell>
+                <TableCell className="flex items-center gap-x-2">
+                  <Link href={`/admin/products/${productId}/edit`}>
+                    <IconButton actionType="edit"></IconButton>
+                  </Link>
+                  <DeleteProduct productId={productId} />
+                </TableCell>
               </TableRow>
             );
           })}
         </TableBody>
       </Table>
     </section>
+  );
+}
+
+// BIND approach - working with forms in JavaScript
+function DeleteProduct({ productId }: { productId: string }) {
+  const deleteProduct = deleteProductAction.bind(null, { productId });
+  return (
+    <FormContainer action={deleteProduct}>
+      <IconButton actionType="delete" />
+    </FormContainer>
   );
 }
 
