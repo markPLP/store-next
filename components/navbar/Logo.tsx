52a176f9a5f819f5caaa0dd533df1fafd371d130
markPLP <mpclaspinas@gmail.com>
2025-01-12 15:58:41 +0800

    animate logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
-import { TbBrandVscode } from 'react-icons/tb';
+// import { TbBrandVscode } from 'react-icons/tb';
+import { FaReact } from 'react-icons/fa';
+import { RiNextjsLine } from 'react-icons/ri';
+
 import { Button } from '../ui/button';
 
 function Logo() {
   return (
-    <Button size="icon" asChild>
+    <Button size="sm" asChild className="bg-transparent hover:bg-transparent">
       <Link href="/">
-        <TbBrandVscode />
+        <FaReact className="text-lg !w-14 !h-14 text-primary animate-[spin_5s_linear_infinite]" />
+        <RiNextjsLine className="text-lg !w-14 !h-14 text-primary" />
       </Link>
     </Button>
   );
